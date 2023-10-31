41797203d798a0a9ae02e7da8bc6ed041d695d76
aszc-dev <kontakt@aszczepanski.pl>
2023-10-31 02:49:35 +0100

    Improve chunking and padding

--- coreml_suite/latents.py
+++ coreml_suite/latents.py
@@ -1,29 +1,34 @@
 import torch
 
+from comfy.model_management import get_torch_device
 
-def chunk_batch(latent_image, target_shape):
-    if latent_image.shape == target_shape:
-        return [latent_image]
 
-    batch_size = latent_image.shape[0]
+def chunk_batch(input_tensor, target_shape):
+    if input_tensor.shape == target_shape:
+        return [input_tensor]
+
+    batch_size = input_tensor.shape[0]
     target_batch_size = target_shape[0]
 
     num_chunks = batch_size // target_batch_size
     if num_chunks == 0:
-        padding = torch.zeros(target_batch_size - batch_size, *target_shape[1:])
-        return [torch.cat((latent_image, padding), dim=0)]
+        padding = torch.zeros(target_batch_size - batch_size, *target_shape[1:]).to(
+            get_torch_device()
+        )
+        return [torch.cat((input_tensor, padding), dim=0)]
 
     mod = batch_size % target_batch_size
     if mod != 0:
-        chunks = list(torch.chunk(latent_image[:-mod], num_chunks))
-        padding = torch.zeros(target_batch_size - mod, *target_shape[1:])
-        padded = torch.cat((latent_image[-mod:], padding), dim=0)
+        chunks = list(torch.chunk(input_tensor[:-mod], num_chunks))
+        padding = torch.zeros(target_batch_size - mod, *target_shape[1:]).to(
+            get_torch_device()
+        )
+        padded = torch.cat((input_tensor[-mod:], padding), dim=0)
         chunks.append(padded)
-        return [chunk for chunk in chunks]
+        return chunks
 
-    chunks = list(torch.chunk(latent_image, num_chunks))
-
-    return [chunk for chunk in chunks]
+    chunks = list(torch.chunk(input_tensor, num_chunks))
+    return chunks
 
 
 def merge_chunks(chunks, orig_shape):
