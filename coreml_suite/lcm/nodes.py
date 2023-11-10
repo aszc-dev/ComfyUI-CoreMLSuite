7492f0b486231800bc9447c14c415b2e660762cd
aszc-dev <kontakt@aszczepanski.pl>
2023-11-10 13:24:06 +0100

    Extract lcm sampler from lcm sampling node

--- coreml_suite/lcm/nodes.py
+++ coreml_suite/lcm/nodes.py
@@ -1,9 +1,21 @@
 import os
 
+import torch
 from coremltools import ComputeUnit
+from diffusers import LCMScheduler
 from python_coreml_stable_diffusion.coreml_model import CoreMLModel
 
+import comfy.utils
+import latent_preview
+from comfy import model_base
+from comfy.model_management import get_torch_device
+from comfy.model_patcher import ModelPatcher
+from coreml_suite.config import get_model_config
 from coreml_suite.lcm import lcm_converter
+from coreml_suite.lcm.sampler import CoreMLSamplerLCM
+from coreml_suite.logger import logger
+from coreml_suite.models import CoreMLModelWrapper
+from coreml_suite.nodes import CoreMLSampler
 
 
 class CoreMLConverterLCM:
@@ -67,3 +79,74 @@ class CoreMLConverterLCM:
         target_path = lcm_converter.compile_model(out_path=out_path, out_name=out_name)
 
         return (CoreMLModel(target_path, compute_unit, "compiled"),)
+
+
+class COREML_SAMPLER_LCM(CoreMLSampler):
+    @classmethod
+    def INPUT_TYPES(s):
+        old_required = CoreMLSampler.INPUT_TYPES()["required"].copy()
+        old_required["steps"][1]["default"] = 4
+        old_required.pop("negative")
+        old_required.pop("sampler_name")
+        old_required.pop("scheduler")
+        new_required = {"coreml_model": ("COREML_UNET",)}
+        return {
+            "required": new_required | old_required,
+            "optional": {"latent_image": ("LATENT",)},
+        }
+
+    CATEGORY = "Core ML Suite"
+
+    def sample(
+        self,
+        coreml_model,
+        seed,
+        steps,
+        cfg,
+        positive,
+        latent_image=None,
+        denoise=1.0,
+        **kwargs,
+    ):
+        scheduler = LCMScheduler.from_pretrained(
+            "SimianLuo/LCM_Dreamshaper_v7", subfolder="scheduler"
+        )
+
+        model_config = get_model_config()
+        wrapped_model = CoreMLModelWrapper(coreml_model)
+        model = model_base.BaseModel(model_config, device=get_torch_device())
+        model.diffusion_model = wrapped_model
+        model_patcher = ModelPatcher(model, get_torch_device(), None)
+
+        if latent_image is None:
+            logger.warning("No latent image provided, using empty tensor.")
+            expected = coreml_model.expected_inputs["sample"]["shape"]
+            latent_image = {"samples": torch.zeros(*expected).to(get_torch_device())}
+
+        x0_output = {}
+        callback = latent_preview.prepare_callback(model_patcher, steps, x0_output)
+        disable_pbar = not comfy.utils.PROGRESS_BAR_ENABLED
+
+        sampler = CoreMLSamplerLCM(scheduler)
+        samples = sampler.sample(
+            model_patcher,
+            seed,
+            steps,
+            cfg,
+            positive,
+            latent_image=latent_image,
+            denoise=denoise,
+            callback=callback,
+            disable_pbar=disable_pbar,
+        )
+
+        out = latent_image.copy()
+        out["samples"] = samples
+        if "x0" in x0_output:
+            out_denoised = latent_image.copy()
+            out_denoised["samples"] = model.process_latent_out(
+                x0_output["x0"].to(get_torch_device())
+            )
+        else:
+            out_denoised = out
+        return out, out_denoised
