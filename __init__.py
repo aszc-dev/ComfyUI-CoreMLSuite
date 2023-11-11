8bcdeab234f390081dedb2af6dfb0dd6cee6e711
aszc-dev <kontakt@aszczepanski.pl>
2023-11-11 03:11:35 +0100

    Core ML Sampler supports LCM

--- __init__.py
+++ __init__.py
@@ -5,7 +5,6 @@ sys.path.append(os.path.dirname(__file__))
 
 from coreml_suite.nodes import CoreMLLoaderUNet, CoreMLSampler, CoreMLModelAdapter
 from coreml_suite.lcm import (
-    COREML_SAMPLER_LCM,
     COREML_CONVERT_LCM,
 )
 
@@ -13,13 +12,11 @@ NODE_CLASS_MAPPINGS = {
     "CoreMLUNetLoader": CoreMLLoaderUNet,
     "CoreMLSampler": CoreMLSampler,
     "CoreMLModelAdapter": CoreMLModelAdapter,
-    "Core ML LCM Sampler": COREML_SAMPLER_LCM,
     "Core ML LCM Converter": COREML_CONVERT_LCM,
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
     "CoreMLUNetLoader": "Load Core ML UNet",
     "CoreMLSampler": "Core ML Sampler",
     "CoreMLModelAdapter": "Core ML Adapter (Experimental)",
-    "Core ML LCM Sampler": "Core ML LCM Sampler",
     "Core ML LCM Converter": "Convert LCM to Core ML",
 }
