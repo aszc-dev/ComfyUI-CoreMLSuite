ae9a9874c549e6e34f85efc6b8b7df760ebce9a1
aszc-dev <kontakt@aszczepanski.pl>
2023-11-19 23:40:44 +0100

    Add Advanced Sampler node

--- __init__.py
+++ __init__.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.dirname(__file__))
 from coreml_suite.nodes import (
     CoreMLLoaderUNet,
     CoreMLSampler,
+    CoreMLSamplerAdvanced,
     CoreMLModelAdapter,
     COREML_CONVERT,
     COREML_LOAD_LORA,
@@ -17,6 +18,7 @@ from coreml_suite.lcm import (
 NODE_CLASS_MAPPINGS = {
     "CoreMLUNetLoader": CoreMLLoaderUNet,
     "CoreMLSampler": CoreMLSampler,
+    "CoreMLSamplerAdvanced": CoreMLSamplerAdvanced,
     "CoreMLModelAdapter": CoreMLModelAdapter,
     "Core ML LoRA Loader": COREML_LOAD_LORA,
     "Core ML Converter": COREML_CONVERT,
@@ -25,6 +27,7 @@ NODE_CLASS_MAPPINGS = {
 NODE_DISPLAY_NAME_MAPPINGS = {
     "CoreMLUNetLoader": "Load Core ML UNet",
     "CoreMLSampler": "Core ML Sampler",
+    "CoreMLSamplerAdvanced": "Core ML Sampler (Advanced)",
     "CoreMLModelAdapter": "Core ML Adapter (Experimental)",
     "Core ML LoRA Loader": "Load LoRA to use with Core ML",
     "Core ML Converter": "Convert Checkpoint to Core ML",
